dd729f82d2f9027d83bc959ed84d4a5f0df9281b
charludo <github@charlotteharludo.com>
2024-09-09 14:25:54 +0200

    Update GTK Materia Theme

--- lib/contrib/gtk-theme.nix
+++ lib/contrib/gtk-theme.nix
@@ -63,9 +63,9 @@ pkgs.stdenv.mkDerivation rec {
 
     echo "Changing colours:"
     ./change_color.sh -o ${scheme.slug} /build/gtk-colors -i False -t "$out/share/themes"
-    cp "$out/gtk-4.0/gtk.css" "$out/gtk-4.0/gtk-dark.css"
-    cp "$out/gtk-3.0/gtk.css" "$out/gtk-3.0/gtk-dark.css"
-    cp "$out/gtk-2.0/gtk.css" "$out/gtk-2.0/gtk-dark.css"
+    cp "gtk-4.0/gtk.css" "gtk-4.0/gtk-dark.css"
+    cp "gtk-3.0/gtk.css" "gtk-3.0/gtk-dark.css"
+    cp "gtk-2.0/gtk.css" "gtk-2.0/gtk-dark.css"
     chmod 555 -R .
   '';
 }
